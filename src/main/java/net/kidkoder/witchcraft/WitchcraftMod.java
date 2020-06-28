be057805efd9f49c0f81d95ce04407c6ff15abff
Kid Koder <codermann09@gmail.com>
2020-06-28 15:43:02 -0500

    Added mod to mod registry. Added RUN MOD.bat so HP35 can test

---
+++ src/main/java/net/kidkoder/witchcraft/WitchcraftMod.java
@@ -0,0 +1,8 @@
+package net.kidkoder.witchcraft;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(WitchcraftMod.MODID)
+public class WitchcraftMod {
+    public static final String MODID = "witchcraft";
+}
